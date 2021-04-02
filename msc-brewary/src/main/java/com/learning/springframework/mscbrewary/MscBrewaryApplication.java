d3f82db8063defb2d60f45132580c099e689bab4
gansh <ganshty9@gmail.com>
2021-04-02 17:43:37 -0400

    Initial Commit

---
+++ msc-brewary/src/main/java/com/learning/springframework/mscbrewary/MscBrewaryApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springframework.mscbrewary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscBrewaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscBrewaryApplication.class, args);
+	}
+
+}
---
+++ msc-brewary/src/test/java/com/learning/springframework/mscbrewary/MscBrewaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springframework.mscbrewary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscBrewaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
